4ca0e9d4b498c0322ff11cbd93a57a98fcb73698
Tingting Duan <duan.tingting@yahoo.com>
2021-06-25 07:59:53 -0500

    resolved conflicts

--- scripts/database.js
+++ scripts/database.js
@@ -1,5 +1,5 @@
 const database = {
-<<<<<<< HEAD
+
     past_projects: [
         {
             id: 1, name: "Hotel Demand Forecast - Machine Learning [Python, SQL, scikit-learn]", 
@@ -22,21 +22,7 @@ const database = {
             webpage: " "
             
         }
-=======
-    past_projects: [{
-        id: 1, name: "Free Meme Generator Web App [React, JS, HTML, CSS]", 
-        link: "https://github.com/Tingting0618/react_app_meme_gen",
-        webpage: "http://localhost:5000/"
-    }, {
-        id: 2, name: "Hotel Demand Forecasting - Machine Learning [Python, SQL]", 
-        link: "https://github.com/Tingting0618/Hotel_Demand_Forecast_Machine_Learning",
-        webpage: null}, 
-        {
-        id: 3, name: "Blackpress Coffee", price: 9.49
-    }
->>>>>>> 562e09cb9cefa9a5b2af57ad655c100a1fc95a08
-    ]
-}
+    ]}
 
 export const getPastProjects = () => {
     return database.past_projects.map(project => ({ ...project }))
